d7cf24a1e4598c49c642d9c546c944213199ac10
Kyouru <ren.kyouru@gmail.com>
2022-11-07 21:36:19 -0500

    20221107

--- APISICA/Controllers/MantenimientoController.cs
+++ APISICA/Controllers/MantenimientoController.cs
@@ -59,10 +59,9 @@ namespace APISICA.Controllers
         }
 
 
-        [HttpPost("crearcuenta")]
-        public IActionResult CrearCuenta(Class.JsonToken jsontoken)
+        [HttpPost("crearusuario")]
+        public IActionResult CrearUsuario(Class.JsonToken jsontoken)
         {
-            DataTable dt;
             Cuenta cuenta;
             string connstr = "";
             try
@@ -102,23 +101,58 @@ namespace APISICA.Controllers
                     conn.iniciaCommand(strSQL);
                     int orden = conn.ejecutarQueryEscalar();
 
-                    strSQL = "INSERT INTO USUARIO (NOMBRE_USUARIO, ID_AREA_FK, REAL, CAMBIAR_PASSWORD, ORDEN, CERRAR_SESION, DATAMANAGER, PASSWORDHASH, PASSWORDSALT, EMAIL, CONNUSER, ANULADO)";
-                    strSQL += " VALUES ('" + jsontoken.descripcion1 + "', " + jsontoken.idaux + ", 1, 1, " + orden + ", 0, 0, '" + _configuration.GetSection("DefaultPassword:hash").Value + "', '" + _configuration.GetSection("DefaultPassword:salt").Value + "', '" + jsontoken.descripcion2 + "', '" + connstr + "', 0)";
+                    strSQL = "INSERT INTO USUARIO (NOMBRE_USUARIO, ID_AREA_FK, REAL, CAMBIAR_PASSWORD, ORDEN, CERRAR_SESION, DATAMANAGER, PASSWORDHASH, PASSWORDSALT, EMAIL, CONNUSER, ANULADO, ACCESO_PERMITIDO)";
+                    strSQL += " VALUES ('" + jsontoken.descripcion1 + "', " + jsontoken.idaux + ", 1, 1, " + orden + ", 0, 0, '" + _configuration.GetSection("DefaultPassword:hash").Value + "', '" + _configuration.GetSection("DefaultPassword:salt").Value + "', '" + jsontoken.descripcion2 + "', '" + connstr + "', 0, 0)";
                     strSQL += " RETURNING ID_USUARIO INTO :numero";
                     int id = conn.InsertReturnID(strSQL);
-
+                    if (id <= 0)
+                    {
+                        return BadRequest("Error Creando Usuario");
+                    }
                     strSQL = "INSERT INTO PERMISO (ID_USUARIO_FK, BUSQUEDA, BUSQUEDA_HISTORICO, BUSQUEDA_EDITAR, ENTREGAR, ENTREGAR_EXPEDIENTE, ENTREGAR_DOCUMENTO, RECIBIR, RECIBIR_NUEVO, RECIBIR_REINGRESO, RECIBIR_CONFIRMAR, RECIBIR_MANUAL, PAGARE, PAGARE_BUSCAR, PAGARE_RECIBIR, PAGARE_ENTREGAR, LETRA, LETRA_NUEVO, LETRA_ENTREGAR, LETRA_REINGRESO, LETRA_BUSCAR, IRONMOUNTAIN, IRONMOUNTAIN_SOLICITAR, IRONMOUNTAIN_RECIBIR, IRONMOUNTAIN_ARMAR, IRONMOUNTAIN_ENVIAR, IRONMOUNTAIN_ENTREGAR, IRONMOUNTAIN_CARGO, BOVEDA, BOVEDA_CAJA_RETIRAR, BOVEDA_CAJA_GUARDAR, BOVEDA_DOCUMENTO_RETIRAR, BOVEDA_DOCUMENTO_GUARDAR, MANTENIMIENTO, MANTENIMIENTO_CUENTA, MANTENIMIENTO_CREDITO, MANTENIMIENTO_SOCIO, IMPORTAR, IMPORTAR_ACTIVAS, IMPORTAR_PASIVAS, NIVEL)";
                     if (jsontoken.idaux == Int32.Parse(_configuration.GetSection("Area:Custodia").Value))
                     {
-                        strSQL += " VALUES (" + id + ", 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 2)";
+                        //ID, BUSQUEDA, BUSQUEDA_HISTORICO, BUSQUEDA_EDITAR
+                        strSQL += " VALUES (" + id + ", 1, 1, 1,";
+                        //ENTREGAR, ENTREGAR_EXPEDIENTE, ENTREGAR_DOCUMENTO
+                        strSQL += " 1, 1, 1,";
+                        //RECIBIR, RECIBIR_NUEVO, RECIBIR_REINGRESO, RECIBIR_CONFIRMAR, RECIBIR_MANUAL
+                        strSQL += " 1, 1, 1, 1, 1,";
+                        //PAGARE, PAGARE_BUSCAR, PAGARE_RECIBIR, PAGARE_ENTREGAR
+                        strSQL += " 1, 1, 1, 1,";
+                        //LETRA, LETRA_NUEVO, LETRA_ENTREGAR, LETRA_REINGRESO, LETRA_BUSCAR
+                        strSQL += " 0, 0, 0, 0, 0,";
+                        //IRONMOUNTAIN, IRONMOUNTAIN_SOLICITAR, IRONMOUNTAIN_RECIBIR, IRONMOUNTAIN_ARMAR, IRONMOUNTAIN_ENVIAR, IRONMOUNTAIN_ENTREGAR, IRONMOUNTAIN_CARGO
+                        strSQL += " 0, 0, 0, 0, 0, 0, 0,";
+                        //BOVEDA, BOVEDA_CAJA_RETIRAR, BOVEDA_CAJA_GUARDAR, BOVEDA_DOCUMENTO_RETIRAR, BOVEDA_DOCUMENTO_GUARDAR
+                        strSQL += " 1, 1, 1, 1, 1,";
+                        //MANTENIMIENTO, MANTENIMIENTO_CUENTA, MANTENIMIENTO_CREDITO, MANTENIMIENTO_SOCIO
+                        strSQL += " 1, 1, 1, 1,";
+                        //IMPORTAR, IMPORTAR_ACTIVAS, IMPORTAR_PASIVAS, NIVEL
+                        strSQL += " 0, 0, 0, 2)";
                     }
                     else
                     {
-                        strSQL += " VALUES (" + id + ", 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3)";
+                        //ID, BUSQUEDA, BUSQUEDA_HISTORICO, BUSQUEDA_EDITAR
+                        strSQL += " VALUES (" + id + ", 1, 1, 0,";
+                        //ENTREGAR, ENTREGAR_EXPEDIENTE, ENTREGAR_DOCUMENTO
+                        strSQL += " 1, 1, 1,";
+                        //RECIBIR, RECIBIR_NUEVO, RECIBIR_REINGRESO, RECIBIR_CONFIRMAR, RECIBIR_MANUAL
+                        strSQL += " 1, 0, 0, 1, 0,";
+                        //PAGARE, PAGARE_BUSCAR, PAGARE_RECIBIR, PAGARE_ENTREGAR
+                        strSQL += " 1, 1, 0, 0,";
+                        //LETRA, LETRA_NUEVO, LETRA_ENTREGAR, LETRA_REINGRESO, LETRA_BUSCAR
+                        strSQL += " 0, 0, 0, 0, 0,";
+                        //IRONMOUNTAIN, IRONMOUNTAIN_SOLICITAR, IRONMOUNTAIN_RECIBIR, IRONMOUNTAIN_ARMAR, IRONMOUNTAIN_ENVIAR, IRONMOUNTAIN_ENTREGAR, IRONMOUNTAIN_CARGO
+                        strSQL += " 0, 0, 0, 0, 0, 0, 0,";
+                        //BOVEDA, BOVEDA_CAJA_RETIRAR, BOVEDA_CAJA_GUARDAR, BOVEDA_DOCUMENTO_RETIRAR, BOVEDA_DOCUMENTO_GUARDAR
+                        strSQL += " 0, 0, 0, 0, 0,";
+                        //MANTENIMIENTO, MANTENIMIENTO_CUENTA, MANTENIMIENTO_CREDITO, MANTENIMIENTO_SOCIO
+                        strSQL += " 0, 0, 0, 0,";
+                        //IMPORTAR, IMPORTAR_ACTIVAS, IMPORTAR_PASIVAS, NIVEL
+                        strSQL += " 0, 0, 0, 2)";
                     }
-
                     conn.ejecutarQuery();
-
                     conn.cerrar();
 
                     return Ok();
